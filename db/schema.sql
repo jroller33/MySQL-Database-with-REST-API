dc0c01604405e851fd370c3a79666a7aded96e97
John Roller <jtroller33@gmail.com>
2022-08-09 20:30:55 -0500

    schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+USE movies_db;
+
+CREATE TABLE movies (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    review TEXT NOT NULL,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (movie_name)
+VALUES ("Lion King"),
+       ("The Godfather"),
+       ("West Side Story"),
+       ("Parasite"),
+       ("The Wizard of Oz");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
+       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
+       (1, "Scar is the lion everyone loves to hate"),
+       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+       (5, "The tin man gave a metallic, hollow performance"),
+       (1, "Hakuna matata"),
+       (5, "Those flying monkeys are nightmare fuel!");
+       
