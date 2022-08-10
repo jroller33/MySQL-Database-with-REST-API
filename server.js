7a1180d38515d1ecee1603a6504faf18b58ac6cd
John Roller <jtroller33@gmail.com>
2022-08-09 20:09:28 -0500

    server started

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// db
+
+// POST for creating movie
+
+// GET all movies
+
+// DELETE movie
+
+// GET
+
+// PUT
+
+
+// 404 not found - default res for any other req
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
