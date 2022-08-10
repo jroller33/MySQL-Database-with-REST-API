03b1dc3cffbb9f4db205a020acb8ef5ded46c921
John Roller <jtroller33@gmail.com>
2022-08-09 20:13:31 -0500

    server.js routes

--- server.js
+++ server.js
@@ -7,10 +7,34 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// db
-
-// POST for creating movie
-
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Aquar1aN^mAG33$0121',  // add mysql password here
+      database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
+  
+  // create a movie
+  app.post('/api/new-movie', ({ body }, res) => {
+    const sql = `INSERT INTO movies (movie_name)
+      VALUES (?)`;
+    const params = [body.movie_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+  
 // GET all movies
 
 // DELETE movie
