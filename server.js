5bb2966232f87d965ac44e19ec9924678e4cd7af
John Roller <jtroller33@gmail.com>
2022-08-09 20:14:25 -0500

    server.js routes almost done

--- server.js
+++ server.js
@@ -35,13 +35,81 @@ const db = mysql.createConnection(
     });
   });
   
-// GET all movies
-
-// DELETE movie
-
-// GET
-
-// PUT
+// read all movies
+app.get('/api/movies', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  
+  // delete a movie
+  app.delete('/api/movie/:id', (req, res) => {
+    const sql = `DELETE FROM movies WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+        message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+  
+  // read list of all reviews and its associated movie name using LEFT JOIN
+  app.get('/api/movie-reviews', (req, res) => {
+    const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  
+  // update review name
+  app.put('/api/review/:id', (req, res) => {
+    const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+    const params = [req.body.review, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+  
 
 
 // 404 not found - default res for any other req
